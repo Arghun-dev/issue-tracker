3c99689df860e136a311bef85085fdee02cfffe5
Arghun <arghava.mousanezhad@utopiamusic.com>
2023-12-07 18:33:15 +0100

    Build create issue API

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+// All this code is doing is creating and exporting a single instance of prisma client, just to make sure that we don't have multiple instances up and running
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
